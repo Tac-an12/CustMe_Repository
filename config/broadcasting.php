14dad72ebe45b90c018eb2d6bdd8da7488c07157
Tac-an12 <marktacan60@gmail.com>
2024-12-16 12:29:41 +0800

    docker updated

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -53,11 +53,11 @@ return [
         'connections' => [
             'pusher' => [
                 'driver' => 'pusher',
-                'key' => env('PUSHER_APP_KEY'),
-                'secret' => env('PUSHER_APP_SECRET'),
-                'app_id' => env('PUSHER_APP_ID'),
+                'key' => env('PUSHER_APP_KEY', 'default_key'),
+                'secret' => env('PUSHER_APP_SECRET', 'default_key'),
+                'app_id' => env('PUSHER_APP_ID', 'default_key'),
                 'options' => [
-                    'cluster' => env('PUSHER_APP_CLUSTER'),
+                    'cluster' => env('PUSHER_APP_CLUSTER', 'default_key'),
                     'useTLS' => true,
                 ],
             ],
